03a3fefb6e467435a0aa475d222b3c7df4aa3bd9
Akshay Shetty <akshayshetty747@gmail.com>
2026-05-20 18:25:29 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://YOUR-VERCEL-APP.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
